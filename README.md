89a3384cee5a55b199f974c6ea596cfd67f3643b
Ralf Bleymehl <ralfbleymehl@gmail.com>
2024-01-22 15:25:46 +0100

    erste implementierungen

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # AuthApi
+
+## Beschreibung
+
+Dieses Projekt ist eine Flask-Api, die eine API bereitstellt, um Berechtigungen für Benutzer basierend auf ihren Windows-Anmeldeinformationen zu verwalten. Die Hauptfunktionen der API umfassen das Generieren eines JWT-Tokens mit dem Windows-Login des Benutzers und das Überprüfen der Berechtigungen dieses Benutzers in der "authapi" Datenbank.
+
+Beispiel Reha:  
+Reha ruft in der Datenbank "authapi" den App Secret Key ab. --> Die Anwendung Reha sendet den App Secret Key zusammen mit der WindowsKennung verschlüsselt mit JWT an die AuthApi App. --> Dort wird der JWT Token entschlüsselt --> Der App Secret Key wird gegengeprüft, ob dieser in der authapi Datenbank steht. --> Falls ja --> Werden die Berechtigungen der App Reha abgefragt und zurück an die App Reha gesendet
+--> Falls nein --> es werden keine Berechtigungen zurück geschickt.
+
+Hintergrund:
+Es soll nur die Applikation auf den URL Pfad zugreifen dürfen, daher wird für jede Applikation ein SECRET KEY in der authapi Datenbank hinterlegt.
+
+## Funktionalitäten
+
+- **JWT-Token-Erstellung**: Generiert einen JWT-Token, der den Windows-Benutzernamen des Benutzers enthält.
+- **Berechtigungsüberprüfung**: Überprüft, ob der Benutzer die notwendigen Berechtigungen hat, um auf bestimmte Ressourcen zuzugreifen.
+
+## Installation
+
+Um das Projekt lokal einzurichten, folgen Sie diesen Schritten:
+
+1. Klonen Sie das Repository:
+   git clone [URL Ihres Git-Repositorys]
+
+2. Wechseln Sie in das Projektverzeichnis:
+   cd [Projektverzeichnis]
+
+3. Erstellen Sie eine virtuelle Umgebung:
+   python -m venv venv
+
+4. Aktivieren Sie die virtuelle Umgebung:
+
+- Windows:
+  ```
+  .\venv\Scripts\activate
+  ```
+- Unix oder MacOS:
+  ```
+  source venv/bin/activate
+  ```
+
+5. Installieren Sie die erforderlichen Pakete:
+
+## Ausführung
+
+Starten Sie die Anwendung mit:
+flask run
--- app.py
+++ app.py
@@ -1,10 +1,44 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_restful import Api, Resource
+import jwt
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/get_permissionUser/', methods=['GET'])
+def get_permissionUser():
+    
+    print(request.environ)
+    # Reha ruft in der Datenbank authapi den App Secret Key ab.
+    # --> Die Anwendung Reha sendet den App Secret Key zusammen mit der WindowsKennung verschlüsselt mit JWT an die AuthApi App.
+    # --> Dort wird der JWT Token entschlüsselt
+    # --> Der App Secret Key wird gegengeprüft, ob dieser in der authapi Datenbank steht.
+    # --> Falls ja --> Werden die Berechtigungen der App Reha abgefragt und zurück an die App Reha gesendet
+    # --> Falls nein --> es werden keine Berechtigungen zurück geschickt.
+    
+    return jsonify({'Hello1' :  'World!'})
+
+def get_permissions():
+
+    #encode = jwt.encode({"user":"sbl2933"},"wurstwasser","HS256")
+
+
+    # TODO
+    # User ruft im Browser die URL auf.
+    # Es wird ein JWT Token generiert mit dem Windowslogin
+    # Dieser wird an das Frontend zurückgegeben und im LocalStorageg gespeichert. Das Frontend zeigt "Weiterleitung an." Der JWT Token im LocalStorage wird an eine neue Route im Backend gesendet 
+    # Dort wird der Token decodiert und man erhält den WindowsLogin. 
+    # In der Funktion wird nun geprüft, über die Datenbank "authapi" ob der WindowsUser aus dem Token die Berechtigung hat, die Berechtigungen aus der Datenbank zusehen.*args
+    # Bei Ja werden die Berechtigungen zurückgegeben.
+    # Hintergrund dieser Methode: Der User könnte eventuell den WindowsLogin Clientseitig manipulieren. Somit könnte er nach der aktuellen Methode die Berechtigungen von anderen WindowsUsers ausgegeben bekommen.
+    
+
+    # Vorerst:
+    # Beim Aufruf der URL wird der WindowsUser ausgelesen
+    # Das Backend prüft in der "authapi" Datenbank, ob der WindowsUser die Berechtigung besitzt die Berechtigungen zu sehen.*args
+    # Falls Ja --> werden diese zurückgegeben
+
+    return jsonify({})
+    #return jsonify({"token":encode})
 
 if __name__ == '__main__':
     app.run(debug=True)
